5b53fea9e5a06e9242091fe216b3892ba764537f
Muhammad Sayyad Amin <97822048+Sayyad-Amin@users.noreply.github.com>
2025-02-24 07:39:31 +0500

    Create generate_password.py

---
+++ generate_password.py
@@ -0,0 +1,8 @@
+import secrets
+import string
+
+length = 16
+chars = string.ascii_letters + string.digits + string.punctuation
+password = ''.join(secrets.choice(chars) for _ in range(length))
+
+print(f"🔑 Generated Password: {password}")
